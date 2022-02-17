6a69072b82483a69045f5fbe78a911c07eb5679e
Ethorbit <hi1212003@gmail.com>
2022-02-16 18:10:47 -0800

    Create docker-container-start.sh

---
+++ docker-container-start.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# This file runs when the docker container starts;
+# however, this was made on image creation;
+# we need to create the runtime srcds script
+# (if it doesn't exist) and then always run it:
+DIR=$(dirname -- $0)
+
+if [ ! -f "$DIR/server/srcds_start.sh" ]; then
+	echo -e "#!/bin/sh\n" >> $DIR/server/srcds_start.sh
+	echo "\"$DIR/server/srcds_run\" $SRCDS_RUN_ARGS" >> $DIR/server/srcds_start.sh
+fi
+
+exec $DIR/server/srcds_start.sh
