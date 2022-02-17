5376fb641796b68fe94938a1a05ffa66d9207ba2
Ethorbit <hi1212003@gmail.com>
2022-02-16 20:53:16 -0800

    Update docker-container-start.sh

--- docker-container-start.sh
+++ docker-container-start.sh
@@ -4,10 +4,10 @@ DIR=$(dirname -- $0)
 if [ ! -f "$DIR/server/update.sh" ]; then
 	echo "Install args: $SRCDS_INSTALL_ARGS"
 	echo -e "#!/bin/sh\n" >> $DIR/server/update.sh
+	echo "steamcmd $SRCDS_INSTALL_ARGS" >> $DIR/server/update.sh
 fi
 
 if [ ! -f "$DIR/server/start.sh" ]; then
-	echo "Run args: $SRCDS_RUN_ARGS"
 	echo -e "#!/bin/sh\n" >> $DIR/server/start.sh
 	echo "\"$DIR/server/srcds_run\" $SRCDS_RUN_ARGS" >> $DIR/server/start.sh
 	chmod +x "$DIR/server/start.sh"
