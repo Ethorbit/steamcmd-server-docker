af2b6d8cb85fa9ebbff1bdeb02c44f245afde448
Ethorbit <hi1212003@gmail.com>
2022-02-16 23:30:51 -0800

    Update docker-container-start.sh

--- docker-container-start.sh
+++ docker-container-start.sh
@@ -2,13 +2,13 @@
 DIR=$(dirname -- $0)
 
 if [ ! -f "$DIR/server/update.sh" ]; then
-	echo -e "#!/bin/sh\n" >> $DIR/server/update.sh
+	echo "#!/bin/sh" >> $DIR/server/update.sh
 	echo "steamcmd +force_install_dir \"$DIR/server\" +login anonymous +app_update $SRCDS_APPID validate +exit" >> $DIR/server/update.sh
 	chmod +x "$DIR/server/update.sh"
 fi
 
 if [ ! -f "$DIR/server/start.sh" ]; then
-	echo -e "#!/bin/sh\n" >> $DIR/server/start.sh
+	echo "#!/bin/sh" >> $DIR/server/start.sh
 	echo "\"$DIR/server/srcds_run\" $SRCDS_RUN_ARGS" >> $DIR/server/start.sh
 	chmod +x "$DIR/server/start.sh"
 fi
