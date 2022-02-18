2e023f759f46febc9eef3c3d6f97ebd7ac3ab1d7
Ethorbit <hi1212003@gmail.com>
2022-02-18 02:15:33 -0800

    Update docker-container-run.sh

--- docker-container-run.sh
+++ docker-container-run.sh
@@ -13,7 +13,7 @@ if [ ! -f "$DIR/server/start.sh" ]; then
 	chmod +x "$DIR/server/start.sh"
 fi
 
-if [[ $SRCDS_AUTOUPDATE -ne "0" ]]; then
+if [[ "$SRCDS_AUTOUPDATE" -ne "0" ]]; then
 	echo "Starting server... (Checking for updates/missing files in the background)"
 	"$DIR/server/update.sh" > /dev/null & "$DIR/server/start.sh"
 fi
