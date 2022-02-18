29889ccc18adae40d211d09e6849c69b4d4700e3
Ethorbit <hi1212003@gmail.com>
2022-02-18 04:57:21 -0800

    Update docker-container-entry.sh

--- docker-container-entry.sh
+++ docker-container-entry.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 DIR=$(dirname -- $0)
 
-if [ ! -z $USER_ID ]; then
+if [ ! -z "$USER_ID" ]; then
     usermod -u $USER_ID srcds > /dev/null
 fi
 
-if [ ! -z $GROUP_ID ]; then
+if [ ! -z "$GROUP_ID" ]; then
     groupmod -g $GROUP_ID srcds > /dev/null
 fi
 
