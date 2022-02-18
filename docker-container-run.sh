f241d071709208740acd04165fa7c0d7c1c4b2c7
Ethorbit <hi1212003@gmail.com>
2022-02-17 19:57:42 -0800

    Update docker-container-run.sh

--- docker-container-run.sh
+++ docker-container-run.sh
@@ -14,6 +14,12 @@ if [ ! -f "$DIR/server/start.sh" ]; then
 	chmod +x "$DIR/server/start.sh"
 fi
 
+if [[ $SRCDS_UPDATE -eq "1" ]]; then
+	echo "Updating server..."
+	"$DIR/server/update.sh"
+fi
+
 echo "Starting server..."
-#"$DIR/server/update.sh"
 "$DIR/server/start.sh"
+
+echo "Testing"
