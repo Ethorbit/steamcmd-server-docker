ab29c3054d002e642ed9dbbfe3f4ba9491890b9c
Ethorbit <hi1212003@gmail.com>
2022-02-18 04:57:06 -0800

    Update docker-container-run.sh

--- docker-container-run.sh
+++ docker-container-run.sh
@@ -2,28 +2,28 @@
 DIR="/home/srcds"
 
 if [ ! -f "$DIR/server/update.sh" ]; then
-	if [ -z $SRCDS_APPID ]; then
+	if [ -z "$SRCDS_APPID" ]; then
 		echo "Can't install! No App ID specified!"
 		exit
 	fi
 
-	echo "#!/bin/sh" >> $DIR/server/update.sh
+	echo "#!/bin/sh" >> "$DIR/server/update.sh"
 	echo "steamcmd +force_install_dir \"$DIR/server\" +login anonymous +app_update $SRCDS_APPID validate +exit" >> "$DIR/server/update.sh"
 	chmod +x "$DIR/server/update.sh"
 fi
 
-if [ ! -z $SRCDS_RUN_ARGS ]; then
+if [ ! -z "$SRCDS_RUN_ARGS" ]; then
 	rm "$DIR/server/start.sh"
 	echo "#!/bin/sh" >> "$DIR/server/start.sh"
 	echo "\"$DIR/server/srcds_run\" $SRCDS_RUN_ARGS" >> "$DIR/server/start.sh"
 	chmod +x "$DIR/server/start.sh"
 fi
 
-if [ -z $SRCDS_AUTOUPDATE ]; then
+if [ -z "$SRCDS_AUTOUPDATE" ]; then
 	SRCDS_AUTOUPDATE=1
 fi
 
-if [ -z $SRCDS_UPDATE ]; then # Manual update is off by default (duh)
+if [ -z "$SRCDS_UPDATE" ]; then # Manual update is off by default (duh)
 	SRCDS_UPDATE=0
 fi
 
