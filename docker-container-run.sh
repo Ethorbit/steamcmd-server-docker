46bc66e664a710b9226dc3f0f70807a867a9e09f
Ethorbit <Ethorbit@protonmail.com>
2022-03-19 00:41:32 -0700

    Update docker-container-run.sh

--- docker-container-run.sh
+++ docker-container-run.sh
@@ -4,7 +4,7 @@ UPDATE_SCRIPT="$DIR/server/update.sh"
 INSTALL_SCRIPT="$UPDATE_SCRIPT"
 START_SCRIPT="$DIR/server/start.sh"
 
-if [ ! -f "$UPDATE_SCRIPT" ]; then
+if [ ! -f "$DIR/server/update.sh" ]; then
 	if [ -z "$SRCDS_APPID" ]; then
 		echo "Can't install! No App ID specified!"
 		exit
@@ -15,7 +15,7 @@ if [ ! -f "$UPDATE_SCRIPT" ]; then
 	chmod +x "$DIR/server/update.sh"
 fi
 
-if [ ! -f "$START_SCRIPT" ] || [ ! -z "$SRCDS_RUN_ARGS" ]; then
+if [ ! -z "$SRCDS_RUN_ARGS" ]; then
 	rm "$DIR/server/start.sh"
 	echo "#!/bin/sh" >> "$DIR/server/start.sh"
 	echo "\"$DIR/server/srcds_run\" $SRCDS_RUN_ARGS" >> "$DIR/server/start.sh"
