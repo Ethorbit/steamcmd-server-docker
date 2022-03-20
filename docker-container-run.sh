418d19744f97d59f3318d5d9bce8d70c94aa8a0d
Ethorbit <Ethorbit@protonmail.com>
2022-03-19 17:01:12 -0700

    Move auto update into its own script

--- docker-container-run.sh
+++ docker-container-run.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 DIR="/home/srcds"
 UPDATE_SCRIPT="$DIR/server/update.sh"
+AUTO_UPDATE_SCRIPT_NAME="srcds-auto-update.sh"
+AUTO_UPDATE_SCRIPT="$DIR/$AUTO_UPDATE_SCRIPT_NAME"
+SRCDS_UPDATE_INTERVAL=86400 # Interval (in seconds)
 INSTALL_SCRIPT="$UPDATE_SCRIPT"
 START_SCRIPT="$DIR/server/start.sh"
-SRCDS_UPDATE_INTERVAL=86400 # Interval (in seconds)
+#AUTO_RESTART_SCRIPT="$DIR/srcds-auto-restart.sh"
 
 if [ ! -f "$UPDATE_SCRIPT" ]; then
 	if [ -z "$SRCDS_APPID" ]; then
@@ -16,12 +19,28 @@ if [ ! -f "$UPDATE_SCRIPT" ]; then
 	chmod +x "$UPDATE_SCRIPT"
 fi
 
+if [ ! -f "$AUTO_UPDATE_SCRIPT" ]; then
+	echo "#!/bin/sh" >> "$AUTO_UPDATE_SCRIPT"
+	echo "while true; do" >> "$AUTO_UPDATE_SCRIPT"
+	echo "nohup "$UPDATE_SCRIPT" &> /dev/null" >> "$AUTO_UPDATE_SCRIPT"
+	echo "sleep \$SRCDS_UPDATE_INTERVAL" >> "$AUTO_UPDATE_SCRIPT"
+	echo "done &" >> "$AUTO_UPDATE_SCRIPT"
+	chmod +x "$AUTO_UPDATE_SCRIPT"
+fi
+
 if [ ! -f "$START_SCRIPT" ]; then
 	rm "$DIR/server/start.sh"
 	echo "#!/bin/sh" >> "$START_SCRIPT"
 	echo "\"$DIR/server/srcds_run\" $SRCDS_RUN_ARGS" >> "$START_SCRIPT"
 	chmod +x "$START_SCRIPT"
 fi
+
+# No need for auto restart, SRCDS Linux does it already.
+# if [ ! -f "$AUTO_RESTART_SCRIPT" ]; then
+# 	# if $(pgrep -c srcds_linux = 0); then
+		# start start.sh again
+	# fi
+# fi
 
 if [ -z "$SRCDS_AUTOUPDATE" ]; then
 	SRCDS_AUTOUPDATE=1
@@ -44,12 +63,7 @@ function install_server {
 
 function start_server_while_updating {
 	echo "Starting server... (Checking for updates and validating files in the background)"
-
-	while true; do
-	        nohup "$UPDATE_SCRIPT" &> /dev/null
-	        sleep $SRCDS_UPDATE_INTERVAL
-	done &
-
+	"$AUTO_UPDATE_SCRIPT"
 	"$START_SCRIPT"
 }
 
@@ -58,6 +72,8 @@ function update_and_start_server {
 	"$UPDATE_SCRIPT"
 	start_server
 }
+
+killall -q "$AUTO_UPDATE_SCRIPT_NAME"
 
 if [ ! -f "$DIR/server/srcds_run" ]; then
 	install_server
