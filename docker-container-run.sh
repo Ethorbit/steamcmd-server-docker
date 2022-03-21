5d15b9ea163c2c9b399bc5984d3bafc6100afb80
Ethorbit <Ethorbit@protonmail.com>
2022-03-20 23:01:01 -0700

    Update docker-container-run.sh

--- docker-container-run.sh
+++ docker-container-run.sh
@@ -2,18 +2,18 @@
 DIR="/home/srcds"
 UPDATE_SCRIPT_NAME="update.sh"
 UPDATE_SCRIPT="$DIR/server/$UPDATE_SCRIPT_NAME"
-#AUTO_UPDATE_SCRIPT_NAME="auto-update.sh"
-#AUTO_UPDATE_SCRIPT="$DIR/server/$AUTO_UPDATE_SCRIPT_NAME"
-#SRCDS_UPDATE_INTERVAL=43200 # Interval (in seconds)
+AUTO_VALIDATE_SCRIPT_NAME="auto-validate.sh"
+AUTO_VALIDATE_SCRIPT="$DIR/server/$AUTO_VALIDATE_SCRIPT_NAME"
+SRCDS_VALIDATE_INTERVAL=43200 # Interval (in seconds)
 INSTALL_SCRIPT="$UPDATE_SCRIPT"
 START_SCRIPT_NAME="start.sh"
 START_SCRIPT="$DIR/server/$START_SCRIPT_NAME"
 #AUTO_RESTART_SCRIPT_NAME="srcds-auto-restart.sh"
 #AUTO_RESTART_SCRIPT="$DIR/$AUTO_RESTART_SCRIPT_NAME"
 
-#if [[ ! $SRCDS_UPDATE_INTERVAL =~ ^-?[0-9]+$ ]]; then
-	#SRCDS_UPDATE_INTERVAL=43200
-#fi
+if [[ ! $AUTO_VALIDATE_INTERVAL =~ ^-?[0-9]+$ ]]; then
+	AUTO_VALIDATE_INTERVAL=43200
+fi
 
 if [ ! -f "$UPDATE_SCRIPT" ]; then
 	if [ -z "$SRCDS_APPID" ]; then
@@ -26,15 +26,14 @@ if [ ! -f "$UPDATE_SCRIPT" ]; then
 	chmod +x "$UPDATE_SCRIPT"
 fi
 
-# Not needed, srcds has auto update functionality
-#if [ ! -f "$AUTO_UPDATE_SCRIPT" ]; then
-	#echo "#!/bin/sh" >> "$AUTO_UPDATE_SCRIPT"
-	#echo "while true; do" >> "$AUTO_UPDATE_SCRIPT"
-	#echo "nohup \"$UPDATE_SCRIPT\" > /dev/null 2>&1" >> "$AUTO_UPDATE_SCRIPT"
-	#echo "sleep $SRCDS_UPDATE_INTERVAL" >> "$AUTO_UPDATE_SCRIPT"
-	#echo "done &" >> "$AUTO_UPDATE_SCRIPT"
-	#chmod +x "$AUTO_UPDATE_SCRIPT"
-#fi
+if [ ! -f "$AUTO_VALIDATE_SCRIPT" ]; then
+	echo "#!/bin/sh" >> "$AUTO_VALIDATE_SCRIPT"
+	echo "while true; do" >> "$AUTO_VALIDATE_SCRIPT"
+	echo "nohup \"$UPDATE_SCRIPT\" > /dev/null 2>&1" >> "$AUTO_VALIDATE_SCRIPT"
+	echo "sleep $SRCDS_VALIDATE_INTERVAL" >> "$AUTO_VALIDATE_SCRIPT"
+	echo "done &" >> "$AUTO_VALIDATE_SCRIPT"
+	chmod +x "$AUTO_VALIDATE_SCRIPT"
+fi
 
 if [ ! -f "$START_SCRIPT" ]; then
 	rm "$DIR/server/start.sh"
@@ -50,13 +49,13 @@ fi
 	# fi
 # fi
 
-# if [ -z "$SRCDS_AUTOUPDATE" ]; then
-# 	SRCDS_AUTOUPDATE=1
-# fi
-#
-# if [ -z "$SRCDS_UPDATE" ]; then # Manual update is off by default (duh)
-# 	SRCDS_UPDATE=0
-# fi
+if [ -z "$SRCDS_VALIDATE" ]; then # Manual update is off by default (duh)
+	SRCDS_VALIDATE=0
+fi
+
+if [ -z "$SRCDS_AUTOVALIDATE" ]; then
+	SRCDS_AUTOVALIDATE=0
+fi
 
 function start_server {
 	echo "Starting server..."
@@ -68,21 +67,21 @@ function install_server {
 	"$INSTALL_SCRIPT"
 }
 
-# function start_server_while_updating {
-# 	#echo "Starting server... (Checking for updates and validating files in the background)"
-# 	#"$AUTO_UPDATE_SCRIPT"
-# 	#"$START_SCRIPT"
-# }
+function start_server_while_updating {
+	echo "Starting server... (Validating files in the background)"
+	"$AUTO_VALIDATE_SCRIPT"
+	"$START_SCRIPT"
+}
 
-# function update_and_start_server {
-# 	#echo "Checking for updates and validating files..."
-# 	#"$UPDATE_SCRIPT"
-# 	start_server
-# }
+function update_and_start_server {
+	echo "Checking for updates and validating files..."
+	"$UPDATE_SCRIPT"
+	start_server
+}
 
 # Prevent overlapping
-#pkill "$AUTO_UPDATE_SCRIPT_NAME"
-#pkill "$UPDATE_SCRIPT_NAME"
+pkill "$AUTO_VALIDATE_SCRIPT_NAME"
+pkill "$UPDATE_SCRIPT_NAME"
 pkill "$START_SCRIPT_NAME"
 pkill "srcds_run"
 
@@ -90,12 +89,12 @@ if [ ! -f "$DIR/server/srcds_run" ]; then
 	install_server
 fi
 
-# if [ "$SRCDS_AUTOUPDATE" != "0" ]; then
-# 	start_server_while_updating
-# else
-# 	if [ "$SRCDS_UPDATE" = "1" ]; then
-# 		update_and_start_server
-# 	fi
-# fi
-
-start_server
+if [ "$SRCDS_AUTOVALIDATE" != "0" ]; then
+	start_server_while_updating
+else
+	if [ "$SRCDS_VALIDATE" = "1" ]; then
+		update_and_start_server
+	else
+		start_server
+	fi
+fi
